d6e1d422d0e044d70e3cb9def774f897306a782f
Sebastiao <rodrigoexer1@gmail.com>
2023-05-31 09:54:54 -0300

    create-interface-historyFinanceCardProps

--- src/components/HistoryFinanceCard/index.tsx
+++ src/components/HistoryFinanceCard/index.tsx
@@ -1,9 +1,23 @@
 import { Container } from "./style";
 
-export function HistoryFinanceCard(){
+interface IHistoryFinanceCardPros{
+    cardColor: string;
+    tagColor: string;
+    title: string;
+    subtitle: string;
+    amount: string;
+}
+
+export function HistoryFinanceCard( 
+    {cardColor, tagColor, title, subtitle, amount} 
+    : IHistoryFinanceCardPros){
     return(
         <Container>
             <h1>HistoryFinanceCard</h1>
+            
+            {cardColor} {tagColor}
+            {title} {subtitle}
+            {amount}
         </Container>
     )
 }
